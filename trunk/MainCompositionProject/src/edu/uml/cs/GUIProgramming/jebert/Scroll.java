8beebd23d8a1493fa90d83422875b81e5e1fa393
jebert5461@gmail.com <jebert5461@gmail.com@2fd17b64-fe91-11dd-88c7-29a3b14d5316>
2009-03-17 22:40:37 +0000

    Added functionality.

--- trunk/MainCompositionProject/src/edu/uml/cs/GUIProgramming/jebert/Scroll.java
+++ trunk/MainCompositionProject/src/edu/uml/cs/GUIProgramming/jebert/Scroll.java
@@ -16,35 +16,50 @@ import java.awt.Dimension;
 import java.awt.Rectangle;
 import javax.swing.BorderFactory;
 import javax.swing.BoundedRangeModel;
-import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JScrollBar;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
- *
- * @author Jim Ebert
+ *  This program demonstrates the functionality of the scroll pane using mouse events
+ *  @author  Jim Ebert, UMass Lowell Computer Science, <a href="mailto:jim.ebert@comcast.net">jim.ebert@comcast.net</a>
+ *  @version 1.1, 2009-3-17
+ *  Last updated: 2008-3-17
  */
 public class Scroll extends javax.swing.JFrame {
 
     /** Size of the area taken up by the new labels */
-    private Dimension area = new Dimension(0,0);    
+    private Dimension area = new Dimension(0,0);
+
+    /** Keeps track of the number of labels */
+    private int numberOfLabels = 0;
 
     /** Creates new form Scroll */
     public Scroll() {
+
+                // set the LookAndFeel to match the IDE
+        // reference: Robinson & Vorobiev p. 728
+        UIManager.LookAndFeelInfo[] info =
+            UIManager.getInstalledLookAndFeels() ;
+
+        int lafno = 3 ;   // desired LookAndFeel for Windows XP
+        try {
+          UIManager.setLookAndFeel( info[lafno].getClassName() ) ;
+        } catch (Exception e) {
+          System.out.println( "Couldn't load " + info[lafno].getClassName() +
+              " look and feel " + e ) ;
+          System.exit( 1 ) ;  // abort run
+        }
         
         initComponents();
 
-//        jpnlHold = new DrawingPane();
-//        jsclShift = new JScrollPane(jpnlHold);
+        // sets the preferred size of the jpanel to (0, 0) so that the scroll
+        // bars are not visible since they are not needed yet
+        jpnlHold.setPreferredSize(new Dimension(0, 0));
 
-        //jsclShift.setPreferredSize(new Dimension(jpnlHold.getWidth() + 10, jpnlHold.getHeight() + 10));
-//        jpnlHold.setPreferredSize(jpnlHold.getPreferredSize());
-        jsclShift.setVerticalScrollBarPolicy
-                (ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-        jsclShift.setHorizontalScrollBarPolicy
-                (ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        // set it so that the user cannot try and scroll w/o scroll bars
+        jbtnScroll.setEnabled(false);
     }
 
     /** This method is called from within the constructor to
@@ -59,7 +74,9 @@ public class Scroll extends javax.swing.JFrame {
         jpnlScrollPaneHolder = new javax.swing.JPanel();
         jsclShift = new javax.swing.JScrollPane();
         jpnlHold = new javax.swing.JPanel();
-        jbtnMove = new javax.swing.JButton();
+        jbtnScroll = new javax.swing.JButton();
+        jtxtLabelToFind = new javax.swing.JTextField();
+        jlblError = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Scroll Practice");
@@ -69,11 +86,16 @@ public class Scroll extends javax.swing.JFrame {
                 jpnlHoldMouseReleased(evt);
             }
         });
+        jpnlHold.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                jpnlHoldComponentResized(evt);
+            }
+        });
 
-        jbtnMove.setText("Move");
-        jbtnMove.addActionListener(new java.awt.event.ActionListener() {
+        jbtnScroll.setText("Scroll to label: ");
+        jbtnScroll.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnMoveActionPerformed(evt);
+                jbtnScrollActionPerformed(evt);
             }
         });
 
@@ -82,16 +104,25 @@ public class Scroll extends javax.swing.JFrame {
         jpnlHoldLayout.setHorizontalGroup(
             jpnlHoldLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jpnlHoldLayout.createSequentialGroup()
-                .addGap(90, 90, 90)
-                .addComponent(jbtnMove)
-                .addContainerGap(249, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(jpnlHoldLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jlblError, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jpnlHoldLayout.createSequentialGroup()
+                        .addComponent(jbtnScroll)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jtxtLabelToFind, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(236, Short.MAX_VALUE))
         );
         jpnlHoldLayout.setVerticalGroup(
             jpnlHoldLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jpnlHoldLayout.createSequentialGroup()
-                .addGap(76, 76, 76)
-                .addComponent(jbtnMove)
-                .addContainerGap(218, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(jpnlHoldLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbtnScroll)
+                    .addComponent(jtxtLabelToFind, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jlblError, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(244, Short.MAX_VALUE))
         );
 
         jsclShift.setViewportView(jpnlHold);
@@ -121,36 +152,29 @@ public class Scroll extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jbtnMoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnMoveActionPerformed
-        JScrollBar bar = jsclShift.getVerticalScrollBar();
-        BoundedRangeModel model = bar.getModel();
-        model.setValueIsAdjusting(true);
-        model.setValue(model.getMinimum());
-
-        model.setValue(0);
-
-        //ScrollHelper pause = new ScrollHelper(1);
-        bar.repaint();
-        //pause.run();
-
-        for(int i = bar.getValue(); i != bar.getVisibleAmount(); ++i) {
-            move(model, i);
-            bar.repaint();
-            //Thread.sleep(10);
-        }
-
-        model.setValueIsAdjusting(false);
-}//GEN-LAST:event_jbtnMoveActionPerformed
-
+    /**
+     * Occurs when the mouse is released
+     * @param evt a java mouse event
+     */
     private void jpnlHoldMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpnlHoldMouseReleased
-        // TODO add your handling code here:
+        jlblError.setText("");
+
+        /**
+         * The update of the area was influenced by:
+         * http://java.sun.com/docs/books/tutorial/uiswing/components
+         * /scrollpane.html
+         */
+
         jsclShift.setVerticalScrollBarPolicy
                 (ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         jsclShift.setHorizontalScrollBarPolicy
                 (ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        final int W = 30;
-        final int H = 30;
+        final int labelWidth = 30;
+        final int labelHeight = 30;
+        /** the extra space added to the height and width if the 
+         rectangle to make it look better*/
+        final int extra = 1;
 
         // holds the x position of the mouse event
         int x ;
@@ -160,53 +184,127 @@ public class Scroll extends javax.swing.JFrame {
         Rectangle rect = new Rectangle();
 
         boolean changed = false;
-        if (SwingUtilities.isRightMouseButton(evt)) {
-            //This will clear the graphic objects.
+        if (SwingUtilities.isRightMouseButton(evt)) {            
+            //clear all the labels the user created
             removeAdded();
             area.width=0;
             area.height=0;
             changed = true;
-        } else {
-            x = evt.getX();// - W/2;
-            y = evt.getY();// - H/2;
+            numberOfLabels = 0;
+        } 
+        else
+        {
+            //add another label
+            ++numberOfLabels;
+
+            x = evt.getX();
+            y = evt.getY();
             if (x < 0) x = 0;
             if (y < 0) y = 0;
-            rect = new Rectangle(x, y, W, H);
+            rect = new Rectangle(x, y, labelWidth, labelHeight);
             
             jpnlHold.scrollRectToVisible(rect);
 
             // check to see if the area of the new labels is
             // greater than the original area
-            int this_width = (x + W + 2);
-            if (this_width > area.width) {
-                area.width = this_width; 
+            int newWidth = (x + labelWidth + extra);
+            if (newWidth > area.width) {
+                area.width = newWidth;
                 changed = true;
             }
 
-            int this_height = (y + H + 2);
-            if (this_height > area.height) {
-                area.height = this_height; 
+            int newHeight = (y + labelHeight + extra);
+            
+            if (newHeight > area.height)
+            {
+                area.height = newHeight;
                 changed = true;
             }
 
             JLabel j = new JLabel();
-            j.setText("New");
+            j.setText( String.valueOf(numberOfLabels));
             j.setBorder(BorderFactory.createEtchedBorder());
             jpnlHold.add(j);
-            j.setBounds(rect);//(evt.getX(), evt.getY(), 30, 30);
+            j.setBounds(rect); // set the size and placement of the new label
         }
         if (changed) {
-            //Update client's preferred size because
-            //the area taken up by the graphics has
-            //gotten larger or smaller (if cleared).
+            //Update preferred size because
+            //the area taken up by the label has
+            //changed.
             jpnlHold.setPreferredSize(area);
 
             //Let the scroll pane know to update itself
             //and its scrollbars.
-            jpnlHold.revalidate();
+            jpnlHold.revalidate();            
         }
         jpnlHold.repaint();
     }//GEN-LAST:event_jpnlHoldMouseReleased
+
+    /**
+     * Done when the scroll to label button is entered
+     * @param evt a java action event
+     */
+    private void jbtnScrollActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnScrollActionPerformed
+        int labelNumber = -1;
+
+        try
+         {
+            labelNumber = Integer.parseInt(jtxtLabelToFind.getText());
+         }
+         catch(Exception e)
+         {
+             jlblError.setText("You must enter a number to look for.");
+             return;
+         }
+         jlblError.setText("");
+
+         if(labelNumber > numberOfLabels || labelNumber <= 0)
+         {
+             jlblError.setText("Invalid label number entered.");
+         }
+         else if(getLoc() == false)
+         {
+             jlblError.setText("Cannot find the indicated label.");                       
+         }
+    }//GEN-LAST:event_jbtnScrollActionPerformed
+
+    /**
+     * Checks to see if the panel has changed size, if it has it will enable or
+     * disable the button to scroll
+     * @param evt a java component event
+     */
+    private void jpnlHoldComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jpnlHoldComponentResized
+        if(jsclShift.getVerticalScrollBar().isShowing() ||
+                    jsclShift.getHorizontalScrollBar().isShowing())
+        {
+            jbtnScroll.setEnabled(true);
+        }
+        else
+        {
+            jbtnScroll.setEnabled(false);
+        }
+    }//GEN-LAST:event_jpnlHoldComponentResized
+    
+    /**
+     * Gets the location of the label the user is looking for
+     * @return true if it has a location, false otherwise
+     */
+    private boolean getLoc()
+    {
+        Component[] c = jpnlHold.getComponents();
+
+        for(int i = 0; i != c.length; ++i)
+        {
+            if(c[i].getClass() == JLabel.class &&
+                    ((JLabel) c[i]).getText().equals(jtxtLabelToFind.getText()))
+            {
+                JLabel j = (JLabel) c[i];
+                jpnlHold.scrollRectToVisible(j.getBounds());
+                return true;
+            }
+        }
+        return false;
+    }
 
     /**
      * Removes the added Labels
@@ -217,19 +315,11 @@ public class Scroll extends javax.swing.JFrame {
 
         for(int i = 0; i != c.length; ++i)
         {
-            if(c[i].getClass() != JButton.class)
+            if(c[i].getClass() == JLabel.class && c[i] != jlblError)
             {
                 jpnlHold.remove(c[i]);
             }
         }
-    }
-
-    private void move(BoundedRangeModel model, int size)
-    {
-        model.setValue(size);
-
-        //ScrollHelper pause = new ScrollHelper(size);
-        //pause.run();
     }
 
     /**
@@ -244,15 +334,17 @@ public class Scroll extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    /** Moves the scroll pane around */
-    private javax.swing.JButton jbtnMove;
+    /** goes to the specific jlabel that is indicated in the test field */
+    private javax.swing.JButton jbtnScroll;
+    /** Displays error messages to the user */
+    private javax.swing.JLabel jlblError;
     /** holds the gui components */
     private javax.swing.JPanel jpnlHold;
     /** holds the scroll pane */
     private javax.swing.JPanel jpnlScrollPaneHolder;
     /** Allows the panel to be scrollable */
     private javax.swing.JScrollPane jsclShift;
+    /** Holds the number of the label to scroll to */
+    private javax.swing.JTextField jtxtLabelToFind;
     // End of variables declaration//GEN-END:variables
-
-
 }
